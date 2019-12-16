e6c6523cd0af50c6054e86fca2e872027ee845d2
razik younes <ryounes.pro@gmail.com>
2019-12-16 21:20:48 +0000

    Update feature_processing.py

--- thierazik/feature_processing.py
+++ thierazik/feature_processing.py
@@ -33,3 +33,16 @@ def change_types(df, int_to_float_columns):
         elif(np.issubdtype(df[column], np.int64) and column not in int_to_float_columns):
             df[column] = df[column].astype("category")
     return df
+    
+
+def add_datepart(df, fldname, drop=True):
+    
+    fld = df[fldname]
+    if not np.issubdtype(fld.dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    for n in ('Year', 'Month', 'Week', 'Day','Hour', 'Dayofweek', 'Dayofyear',
+            'Is_month_end', 'Is_month_start', 'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 'Is_year_start'):
+        df[targ_pre+n] = getattr(fld.dt,n.lower())
+    df[targ_pre+'Elapsed'] = fld.astype(np.int64) // 10**9
+    if drop: df.drop(fldname, axis=1, inplace=True)
